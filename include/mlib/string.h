16dd928d388441b2ea666ed552cfaab6f1fcf2b7
daynizm2000 <vladikavkazvladivostok@gmail.com>
2026-07-27 22:18:15 +0400

    add: API description

--- include/mlib/string.h
+++ include/mlib/string.h
@@ -2,6 +2,82 @@
 
 
 
+
+/*
+----FULL-API----
+
+Data types:
+- mlib_str_t          Main string type
+- mlib_str_attr_t     String attributes (memory operations, private data)
+- mlib_str_literal_t  Lightweight string literal type
+
+Macros:
+- mlib_str_for_each(obj, p)                    Forward iteration
+- mlib_str_for_each_index(obj, idx)            Forward index iteration  
+- mlib_str_for_each_reverse(obj, p)            Reverse iteration
+- mlib_str_for_each_index_reverse(obj, idx)    Reverse index iteration
+- mlib_str_prv_data(obj)                       Access private data
+
+Literal operations:
+- mlib_str_literal_set(obj, literal)     Set literal string
+- mlib_str_literal_len(obj)              Get literal length
+- mlib_str_literal_data(obj)             Get literal data pointer
+- mlib_str_literal_clear(obj)            Clear literal string
+
+Construction:
+- mlib_str_init(obj, data, attr)         Initialize string
+- mlib_str_destroy(obj)                  Destroy string
+
+Capacity:
+- mlib_str_reserve(obj, newcap)          Reserve capacity
+- mlib_str_resize(obj, newsize, fill)    Resize string
+- mlib_str_shrink_to_fit(obj)            Reduce capacity to fit
+
+Element access:
+- mlib_str_data(obj)                     Get data pointer
+- mlib_str_len(obj)                      Get string length  
+- mlib_str_at(obj, idx)                  Access character at index
+- mlib_str_front(obj)                    Access first character
+- mlib_str_back(obj)                     Access last character
+- mlib_str_empty(obj)                    Check if empty
+
+Modifiers:
+- mlib_str_clear(obj)                    Clear string
+- mlib_str_set(obj, data)                Set string content
+- mlib_str_set_fmt(obj, fmt, ...)        Set with format
+- mlib_str_cat(obj, data)                Concatenate string
+- mlib_str_cat_fmt(obj, fmt, ...)        Concatenate with format
+- mlib_str_push(obj, val)                Push character
+- mlib_str_pop(obj)                      Pop character
+- mlib_str_insert(obj, idx, data)        Insert string at position
+- mlib_str_erase(obj, start, end)        Erase range
+- mlib_str_replace(obj, old, new)        Replace substring
+- mlib_str_swap(obj1, obj2)              Swap two strings
+- mlib_str_copy(dst, src)                Copy string
+- mlib_str_move(dst, src)                Move string
+
+String operations:
+- mlib_str_cmp(obj1, obj2)               Compare strings
+- mlib_str_case_cmp(obj1, obj2)          Case-insensitive compare
+- mlib_str_find(obj, data)               Find substring
+- mlib_str_rfind(obj, data)              Find substring from end
+- mlib_str_find_char(obj, data)          Find character
+- mlib_str_rfind_char(obj, data)         Find character from end
+- mlib_str_startswith(obj, prefix)       Check prefix
+- mlib_str_endswith(obj, suffix)         Check suffix
+
+Transformation:
+- mlib_str_toupper(obj)                  Convert to uppercase
+- mlib_str_tolower(obj)                  Convert to lowercase
+
+----------------
+*/
+
+
+
+
+
+
 #include <stddef.h>
 #include <sys/types.h>
 #include <stdbool.h>
